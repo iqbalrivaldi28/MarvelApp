a60c53424294a8eb59546efd73926e6a268f61c7
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-03-27 22:20:44 +0700

    feat: add logcat message

--- app/src/main/java/com/example/marvelapp/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.marvelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.marvelapp.data.MainModel
+import com.example.marvelapp.retrofit.ApiService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+        getDataFromApi()
+    }
+
+    private fun getDataFromApi(){
+        ApiService.endpoint.getPhotos()
+            .enqueue(object : Callback<List<MainModel>>{
+                override fun onResponse(
+                    call: Call<List<MainModel>>,
+                    response: Response<List<MainModel>>
+                ) {
+                    if (response.isSuccessful){
+                        val result = response.body()
+                        showPhotos(result!!)
+                    }
+                }
+
+                override fun onFailure(call: Call<List<MainModel>>, t: Throwable) {
+                    printLog(t.toString())
+                }
+
+            })
+    }
+
+    private fun printLog(message: String){
+        Log.d(TAG, message)
+    }
+
+    private fun showPhotos(photos: List<MainModel>){
+        for (photo in photos){
+            printLog("title: ${photo.title  }")
+        }
+    }
+
 }
