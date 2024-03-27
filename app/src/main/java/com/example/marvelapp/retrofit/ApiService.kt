88ee120933d85062116ab98cb2d9ce5e13da433e
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-03-28 06:06:49 +0700

    feat: add OkHttp Interceptor

--- app/src/main/java/com/example/marvelapp/retrofit/ApiService.kt
+++ app/src/main/java/com/example/marvelapp/retrofit/ApiService.kt
@@ -1,5 +1,7 @@
 package com.example.marvelapp.retrofit
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -7,8 +9,17 @@ object ApiService {
     val BASE_URL: String = "https://demo.lazday.com/rest-api-sample/"
     val endpoint: ApiEndpoint
         get() {
+
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build()
+
             val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
