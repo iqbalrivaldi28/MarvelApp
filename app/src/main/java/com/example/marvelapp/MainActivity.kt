d58b27c51e314de1d00ec77d04c7e2000f7daa50
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-03-28 07:06:43 +0700

    feat: add RecylerView and Glide Image

--- app/src/main/java/com/example/marvelapp/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.marvelapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.marvelapp.data.MainModel
 import com.example.marvelapp.retrofit.ApiService
 import retrofit2.Call
@@ -13,29 +18,49 @@ class MainActivity : AppCompatActivity() {
 
     private val TAG: String = "MainActivity"
 
+    private lateinit var mainAdapter: MainAdapter
+
+    private lateinit var progressBar: ProgressBar
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progressBar = findViewById(R.id.progressBar)
     }
 
     override fun onStart() {
         super.onStart()
+        setupRecylerView()
         getDataFromApi()
     }
 
+    private fun setupRecylerView() {
+        mainAdapter = MainAdapter(arrayListOf())
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.apply{
+            layoutManager = LinearLayoutManager(applicationContext)
+            adapter = mainAdapter
+        }
+    }
+
     private fun getDataFromApi(){
+        progressBar.visibility = View.VISIBLE
         ApiService.endpoint.getData()
             .enqueue(object : Callback<MainModel>{
                 override fun onResponse(
                     call: Call<MainModel>,
                     response: Response<MainModel>
                 ) {
+                    progressBar.visibility = View.GONE
                     if (response.isSuccessful){
                         showData(response.body()!!)
                     }
                 }
 
                 override fun onFailure(call: Call<MainModel>, t: Throwable) {
+                    progressBar.visibility = View.GONE
                     printLog(t.toString())
                 }
 
@@ -48,9 +73,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun showData(data: MainModel){
         val results = data.result
-        for (result in results){
-            printLog("title: ${result.title}")
-        }
+        mainAdapter.setData(results)
+//        for (result in results){
+//            printLog("title: ${result.title}")
+//        }
     }
 
 }
---
+++ app/src/main/java/com/example/marvelapp/MainAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.marvelapp
+
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.marvelapp.data.MainModel
+
+class MainAdapter(val results: ArrayList<MainModel.Result>) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+    class ViewHolder(val view : View): RecyclerView.ViewHolder(view){
+        val textView: TextView = view.findViewById(R.id.textView)
+        val imageView: ImageView = view.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =  ViewHolder (
+        LayoutInflater.from(parent.context).inflate(R.layout.adapter_main, parent, false)
+
+    )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val result = results[position]
+        holder.textView.text = result.title
+        Glide.with(holder.view)
+            .load(result.image)
+            .placeholder(R.drawable.img_placeholder)
+            .error(R.drawable.img_placeholder)
+            .centerCrop()
+            .into(holder.imageView)
+    }
+
+    override fun getItemCount() = results.size
+
+    fun setData(data: List<MainModel.Result>){
+        results.clear()
+        results.addAll(data)
+        notifyDataSetChanged()
+    }
+
+}
